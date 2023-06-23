ed0e69cc9424f0a02023f64a27e638144f2c3a60
Mohammad Subkhan <subkhanpinter22@gmail.com>
2023-06-23 17:38:16 +0700

    feat: seeder Interest

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(PlaceSeeder::class);
+        $this->call(InterestSeeder::class);
+        // $this->call(PlaceSeeder::class);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
--- database/seeders/InterestSeeder.php
+++ database/seeders/InterestSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Interest;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,15 @@ class InterestSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $csvFile = fopen(__DIR__ . '/interest.csv', 'r');
+        while (($data = fgetcsv($csvFile, 1000, ",")) !== FALSE) {
+
+            $interest = new Interest();
+            $interest->id = $data[0];
+            $interest->name = $data[1];
+            $interest->save();
+
+        } 
+        fclose($csvFile);
     }
 }
